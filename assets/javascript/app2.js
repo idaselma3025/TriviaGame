664ed88de72a8736bd180fdf93e07db42015172b
idaselma3025 <idaselma3025@gmail.com>
2017-12-18 21:20:44 -0600

    betterbuttons

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,63 +1,10 @@
-
-//VARIABLES
-var number = 10; //variable to set countdown number
-var intervalId; //variable to hold decremented countdown number
-var questionIndex = 0;//variable to hold current index of guesses array
-var newGuess; //variable to hold newly created button to hold on item from the "guesses" Array
-var winCounter=0; //variable to hold number of wins during round
-var lossCounter=0; //variable to hold number of losses during round
-var unansweredCounter=0;// variable to hold number o timeouts during round
-var questions = [
-  {
-    "question":"What the name of Queen Elizabeth's sister?",
-    "guesses":["Mary","Jane","Wilemina","Margaret"],
-    "answer":"Margaret"
-  },
-  {
-    "question":"How many children did Queen Elizabeth have?",
-    "guesses":["four","none","two","three"],
-    "answer":"four"
-  },
-  {
-    "question":"What year was Queen Elizabeth II crowned?",
-    "guesses":["1953","1960","1949","1957"],
-    "answer":"1953"
-  },
-  {
-    "question":"What was the Queen's role in WWII?",
-    "guesses":["Queen","mechanic","ambassador","medic"],
-    "answer":"mechanic"
-  },
-  {
-    "question":"Where does the Queen and her family live?",
-    "guesses":["Buckingham Palace","King's Landing","Windsor Castle","Winterfell"],
-    "answer":"Buckingham Palace"
-  },
-]; //question array
-
-//getting Started function
-$(".start").on("click",function(){
-  $(".main").show();
-  $(".start").hide();
-  giantObject.renderQuestion();
-})
-$(document).on("click",".replay",function(){
-  giantObject.clearScreen();
-  $("#answers").empty();
-  questionIndex = 0;
-  giantObject.renderQuestion();
-  winCounter = 0;
-  lossCounter = 0;
-  unansweredCounter = 0;
-})
-
 var giantObject = {
   decrement: function (){
     number--;
     $("#countDown").html("<h2>" + number + "</h2>");
     if (number === 0) {
       ++unansweredCounter;
-      giantObject.nextQuestion();
+      nextQuestion();
       $("#question").text("Out of Time!");
     }
   },
@@ -106,9 +53,9 @@ var giantObject = {
     var winScore =$("<div>");
     var lossScore =$("<div>");
     var unansweredScore =$("<div>");
-    var playAgain = $("<button>");
-    playAgain.addClass("replay");
+    var playAgain = $("<button>")
     playAgain.text("Press to Play Again!");
+    playAgain.addClass("replay");
     winScore.text("Correct Answers: " +winCounter);
     lossScore.text("Wrong Answers: " + lossCounter);
     unansweredScore.text("Timeout Answers: " +unansweredCounter);
@@ -117,7 +64,7 @@ var giantObject = {
   nextQuestion: function(){
     giantObject.stop();
     giantObject.clearScreen();
-    setTimeout(giantObject.renderQuestion,3000);
+    setTimeout(renderQuestion,3000);
     number = 10;
   },
   stop: function(){
