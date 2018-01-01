4a085ec3bdb540fd8d3420844c9ed78294cb1d17
idaselma3025 <idaselma3025@gmail.com>
2018-01-01 10:07:54 -0600

    videorefactor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,6 @@
 
 //VARIABLES
-var number = 10; //variable to set countdown number
-var intervalId; //variable to hold decremented countdown number
-var questionIndex = 0;//variable to hold current index of guesses array
 var newGuess; //variable to hold newly created button to hold on item from the "guesses" Array
-var winCounter=0; //variable to hold number of wins during round
-var lossCounter=0; //variable to hold number of losses during round
-var unansweredCounter=0;// variable to hold number o timeouts during round
-var searchAnswer;//variable to hold current correct answer
-var giphyAnswer; //variable to hold the array item of the giphyId
 var questions = [
   {
     "question":"What was the name of Queen Elizabeth's sister?",
@@ -41,121 +33,101 @@ var questions = [
     "giphyId":["3o7528Oqfsq6cyyjHW","cv6XZnaAjV408"]
   },
 ]; //question array
+
 //getting Started function
 $(".start").on("click",function(){
   $(".main").show();
   $(".start").hide();
-  giantObject.renderQuestion();
+  game.renderQuestion();
 })
 $(document).on("click",".replay",function(){
-  giantObject.clearScreen();
-  $("#answerList").empty();
-  $("#answerScore").empty();
-  questionIndex = 0;
-  giantObject.renderQuestion();
-  winCounter = 0;
-  lossCounter = 0;
-  unansweredCounter = 0;
+  game.reset();
 })
-
-var giantObject = {
-  decrement: function (){
-    number--;
-    $("#countDownNum").html(number);
-    if (number === 0) {
-      ++unansweredCounter;
-      giantObject.nextQuestion();
-      searchAnswer = questions[questionIndex-1].answer;
-      $("#question").text("Out of Time! Too slow, peasant! The correct answer was: " +  searchAnswer+".");
-      giphyAnswer = questions[questionIndex-1].giphyId[0];
-      giantObject.searchGiphy(giphyAnswer);
+$(document).on("click",".button",function(e){
+  game.findAnswers(e);
+})
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:10,
+  winCounter:0,
+  lossCounter:0,
+  unansweredCounter:0,
+  decrement: function(){
+    game.counter--;
+    $("#countDownNum").html(game.counter);
+    if(game.counter === 0){
+      game.timeUp();
     }
-  },
-  run: function (){
-    intervalId = setInterval(giantObject.decrement, 1000);
   },
   renderQuestion: function(){
-    if (questionIndex <= (questions.length-1)){
-      $("#question").text(questions[questionIndex].question);
-      $("#countDownNum").html("10");
-      giantObject.run();
-      giantObject.createButtons();
-      giantObject.findAnswers();
-    }
-    else{
-      giantObject.clearScreen();
-      giantObject.endGame();
+    $("#countDownNum").html(game.counter);
+    timer = setInterval(game.decrement,1000);
+    $("#question").html(questions[game.currentQuestion].question);
+    game.createButtons();
+
+  },
+  timeUp: function(){
+    $(".button").remove();
+    clearInterval(timer);
+    game.unansweredCounter++;
+    $("#question").text("Out of Time! Too slow, peasant! The correct answer was: " + questions[game.currentQuestion].answer +".");
+    game.searchGiphy(questions[game.currentQuestion].giphyId[0]);
+    if(game.currentQuestion===questions.length-1){
+      setTimeout(game.endGame, 3000);
+    } else{
+      setTimeout(game.nextQuestion,3000);
     }
   },
   createButtons: function (){
-    $(".gifs").remove();
-    for (var i = 0; i<questions[questionIndex].guesses.length; i++){
+    for (var i = 0; i<questions[game.currentQuestion].guesses.length; i++){
       newGuess = $("<li>");
       newGuess.addClass("button");
-      newGuess.attr("data-q", questions[questionIndex].guesses[i]);
-      newGuess.text(questions[questionIndex].guesses[i]);
+      newGuess.attr("data-q", questions[game.currentQuestion].guesses[i]);
+      newGuess.text(questions[game.currentQuestion].guesses[i]);
       $("#answerList").append(newGuess);
-      giantObject.hoverButtons();
+      game.hoverButtons();
     }
   },
-  hoverButtons : function(){
-    $(".button").hover(function(){
-      $(this).css("background-color","midnightblue")
-    }, function(){
-      $(this).css("background-color","firebrick")
-  });
-},
-  findAnswers: function(){
-    $(".button").on("click",function(){
-      var tryGuess = ($(this).attr("data-q"));
-      if (tryGuess === questions[questionIndex].answer){
-        ++winCounter;
-        giantObject.nextQuestion();
-        $("#question").text("Correct! Well done, loyal subject!");
-        giphyAnswer = questions[questionIndex-1].giphyId[1];
-        giantObject.searchGiphy(giphyAnswer);
-
-      }
-      else if (tryGuess != questions[questionIndex].answer){
-        ++lossCounter;
-        giantObject.nextQuestion();
-        searchAnswer = questions[questionIndex-1].answer;
-        $("#question").text("Wrong! Try harder, plebeian. The correct answer was: " + searchAnswer+".");
-        giphyAnswer = questions[questionIndex-1].giphyId[0];
-        giantObject.searchGiphy(giphyAnswer);
-      }
-    });
+  findAnswers: function(e){
+    clearInterval(timer);
+    if ($(e.target).data("q") === questions[game.currentQuestion].answer){
+      game.answeredCorrectly();
+    }
+    else if ($(e.targt).data("q")!= questions[game.currentQuestion].answer){
+      game.answeredIncorrectly();
+    }
   },
-  endGame: function(){
-    $("#question").text("All Done Here's How You Did");
-    var winScore =$("<div>");
-    var lossScore =$("<div>");
-    var unansweredScore =$("<div>");
-    var playAgain = $("<button>");
-    playAgain.addClass("replay");
-    playAgain.text("Press to Play Again!");
-    winScore.addClass("scores");
-    winScore.text("Correct Answers: " +winCounter);
-    lossScore.text("Wrong Answers: " + lossCounter);
-    lossScore.addClass("scores");
-    unansweredScore.text("Timeout Answers: " +unansweredCounter);
-    unansweredScore.addClass("scores");
-    $("#answerScore").append(winScore,lossScore, unansweredScore, playAgain);
+  answeredCorrectly: function(){
+    $(".button").remove();
+    clearInterval(timer);
+    game.winCounter++;
+    $("#question").text("Correct! Well done, loyal subject!");
+    game.searchGiphy(questions[game.currentQuestion].giphyId[1]);
+    if(game.currentQuestion==questions.length-1){
+      setTimeout(game.endGame, 3000);
+    } else{
+      setTimeout(game.nextQuestion,3000);
+    }
+  },
+  answeredIncorrectly: function(){
+    $(".button").remove();
+    clearInterval(timer);
+    game.lossCounter++;
+    $("#question").text("Wrong! Try harder, plebeian. The correct answer was: " + questions[game.currentQuestion].answer+".");
+    game.searchGiphy(questions[game.currentQuestion].giphyId[0]);
+    if(game.currentQuestion==questions.length-1){
+      setTimeout(game.endGame, 3000);
+    } else{
+      setTimeout(game.nextQuestion,3000);
+    }
   },
   nextQuestion: function(){
-    giantObject.stop();
-    giantObject.clearScreen();
-    setTimeout(giantObject.renderQuestion,5000);
-  },
-  stop: function(){
-    number=10;
-    clearInterval(intervalId);
-    ++questionIndex;
-  },
-  clearScreen: function(){
     $("#question").empty();
-    $(".button").remove();
     $(".gifs").remove();
+    game.counter = 10;
+    game.currentQuestion++;
+    game.renderQuestion();
   },
   searchGiphy : function(giphyAnswer){
     var queryURL = "https://api.giphy.com/v1/gifs/" + giphyAnswer + "?api_key=H5NnYWud8bpvU4ICC178EnuAHbGH056M";
@@ -167,5 +139,43 @@ var giantObject = {
       imgDiv.addClass("gifs");
       $("#answers").append(imgDiv);
     })
-  }
+  },
+  endGame: function(){
+    $(".gifs").remove();
+    clearInterval(timer);
+    $("#question").text("All Done Here's How You Did");
+    var winScore =$("<div>");
+    var lossScore =$("<div>");
+    var unansweredScore =$("<div>");
+    var playAgain = $("<button>");
+    playAgain.addClass("replay");
+    playAgain.text("Press to Play Again!");
+    winScore.addClass("scores");
+    winScore.text("Correct Answers: " + game.winCounter);
+    lossScore.text("Wrong Answers: " + game.lossCounter);
+    lossScore.addClass("scores");
+    unansweredScore.text("Timeout Answers: " +game.unansweredCounter);
+    unansweredScore.addClass("scores");
+    $("#answerScore").append(winScore,lossScore, unansweredScore, playAgain);
+  },
+  reset:function(){
+    $("#question").empty();
+    $(".button").remove();
+    $(".gifs").remove();
+    $("#answerList").empty();
+    $("#answerScore").empty();
+    game.currentQuestion = 0;
+    game.counter=10;
+    game.renderQuestion();
+    game.winCounter = 0;
+    game.lossCounter = 0;
+    game.unansweredCounter = 0;
+  },
+  hoverButtons : function(){
+   $(".button").hover(function(){
+     $(this).css("background-color","midnightblue")
+   }, function(){
+     $(this).css("background-color","firebrick")
+ })
+}
 };
